d43890d40e8650707a50be2437f04867605d94aa
Paul Kravchenko <pk@sitecore.net>
2018-06-04 15:56:28 +0300

    GetContactFilterExpression method extend with filtering by Contact's LastModified value

---
+++ src/Sitecore.Support.223253/ReadXConnectContactsStepProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+using Sitecore.DataExchange.Contexts;
+using Sitecore.DataExchange.Models;
+using Sitecore.DataExchange.Plugins;
+using Sitecore.XConnect;
+using Sitecore.DataExchange.Extensions;
+
+namespace Sitecore.Support.DataExchange.Providers.XConnect.Processors.PipelineSteps
+{
+  public class ReadXConnectContactsStepProcessor: Sitecore.DataExchange.Providers.XConnect.Processors.PipelineSteps.ReadXConnectContactsStepProcessor
+  {
+    protected override  Expression<Func<Contact, bool>> GetContactFilterExpression(PipelineStep pipelineStep, PipelineContext pipelineContext,
+      Sitecore.Services.Core.Diagnostics.ILogger logger)
+    {
+      ParameterExpression expression;
+      var baseExpression = base.GetContactFilterExpression(pipelineStep, pipelineContext, logger);
+      var drExpression = this.GetDateRangeExpression(pipelineContext, out expression);
+
+      ParameterExpression[] parameters = new ParameterExpression[] { expression };
+
+      return Expression.Lambda<Func<Contact, bool>>(Expression.And(baseExpression?.Body??Expression.Constant(true), drExpression), parameters);
+    }
+
+    protected virtual Expression GetDateRangeExpression(PipelineContext pipelineContext, out ParameterExpression expression)
+    {
+      var lastmodified = Expression.Convert(Expression.PropertyOrField(expression = Expression.Parameter(typeof(Contact), "contact"), "LastModified"), typeof(DateTime));
+      var drplugin = pipelineContext.GetPlugin<DateRangeSettings>();
+      if (drplugin == null)
+      {
+        return Expression.Constant(true);
+      }
+
+      var lowerDateUtc = drplugin.LowerBound.ToUniversalTime();
+      var upperDateUtc = drplugin.UpperBound.ToUniversalTime();
+
+      var lowerBound = Expression.Constant(lowerDateUtc);
+      var upperBound = Expression.Constant(upperDateUtc);
+      var drExpression = Expression.And(Expression.GreaterThan(lastmodified, lowerBound), Expression.LessThan(lastmodified, upperBound));
+      return drExpression;
+    }
+  }
+}
